a3434986fd7d7d8283113afc83748cc207582d19
Anna Shalaginova <annashalaginova@PYMMAC36.local>
2019-03-19 11:37:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,29 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+/*console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);*/
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+cardOne = cards[0];
+cardsInPlay.push(cardOne)
+console.log("User flipped queen");
+cardsInPlay = [];
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo)
+console.log("User flipped king");
+
+var cardsInPlay = [];
+
+ function checkForMatch() {
+  if (cardsInPlay.length == 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+    
+
+      } else {
+          alert("Sorry, try again");
+        };
+
+  };
+};
+
